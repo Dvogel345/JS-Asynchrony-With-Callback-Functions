54150cd38a2a83cf33f586601a04671b9492dc08
Derek Vogel <78238679+Dvogel345@users.noreply.github.com>
2021-11-06 13:55:00 -0400

    added new path of npc

--- index.js
+++ index.js
@@ -5,13 +5,34 @@ const pc = newPlayableCharacter(100, 110)
 const npc = newNonPlayableCharacter(50, 300)
 
 // have the NPC start walking east immediately
-npc.walkEast(500, () => {
-    // then walk north
-    npc.walkNorth(500, ()=>{
-        // then walk west
-        npc.walkWest(500, ()=>{
-            // then walk south and stop
-            npc.walkSouth(500, stop())
+// npc.walkEast(500, () => {
+//     // then walk north
+//     npc.walkNorth(500, ()=>{
+//         // then walk west
+//         npc.walkWest(500, ()=>{
+//             // then walk south and stop
+//             npc.walkSouth(500, stop())
+//         })
+//     })
+// })
+
+// have NPC start walking north for 1400ms
+npc.walkNorth(1400, ()=>{
+    // have NPC start walking east for 1200ms
+    npc.walkEast(1200, ()=>{
+        // have NPC start walking south for 300ms
+        npc.walkSouth(300, ()=>{
+            // have NPC start walking east for 1500ms
+            npc.walkEast(1500, ()=>{
+                // have NPC start walking south for 1500ms
+                npc.walkSouth(1500, ()=>{
+                    // have NPC start walking west for 2700ms
+                    npc.walkWest(2700, ()=>{
+                        // have NPC start walking north for 400ms then stop
+                        npc.walkNorth(400, stop())
+                    })
+                })
+            })
         })
     })
 })
